5f55b02ddd0ca22c0f4fe32d0874806de385563d
zajaffry <93837343+zajaffry@users.noreply.github.com>
2021-11-23 19:51:35 -0500

    Update Connect_4.cpp

--- Connect_4.cpp
+++ Connect_4.cpp
@@ -1,214 +1,210 @@
+//
+// Created by zainj on 11/23/2021.
+//
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+int table4 [6][7];
 
 
 
-class Player{
-private:
-    int ID;
-    int pieces;
+class Table {
 public:
 
-    Player (int i, int p = 21){
-        ID = i;
+    virtual void initialize () = 0;
+    virtual bool check (int ID) = 0;
+    virtual void place (int ID) = 0;
+
+};
+
+class Table4 : public Table {
+private:
+    int id;
+    int pieces = 42;
+public:
+    Table4(int ID = 0, int p = 42) {
+        id = ID;
         pieces = p;
     }
-
-    int get_ID () {
-        return ID;
+    int get_ID(){
+        return id;
     }
-    int get_pieces (){
+    int get_pieces(){
         return pieces;
     }
-
-    Player& operator -- (int a){
+    Table4& operator -- (int a){
         pieces = pieces - 1;
         return *this;
     }
-
-
-
-};
-class Table {
-private:
-    int * table [6][7];
-public:
-
-    Table& = (Player a){
-        table [0][0] = a.get_ID;
-    }
-
-};
-void initialize () {
-
-};
-
-void print(Player * t[6][7]){
-    int x = 0;
-    while(x < 6) {
-        int y = 0;
-        while(y < 7){
-            cout  << t[x][y]->get_ID() << " ";
-            y++;
+    void print (){
+        int x = 0;
+        while(x < 6) {
+            int y = 0;
+            while(y < 7){
+                cout  << table4[x][y] << " ";
+                y++;
+            }
+            cout << endl;
+            x++;
         }
         cout << endl;
-        x++;
+
     }
 
-}
+    void initialize (){
+        int x = 0;
+        int p = 21;
+        Table4 emp (0,0);
+        while(x < 6){
+            int y = 0;
+            while(y < 7){
+                table4[x][y] = 0;
+                y++;
+            }
+            x++;
+        }
 
-bool check (int ID){
-    // horizontal
-    for (int i = 0; i < 6; i++){
-        for (int j = 0; j < 4; j++){
-            if((table[i][j]->get_ID() == ID) && (table[i][j+1]->get_ID() == ID) && (table[i][j+2]->get_ID() == ID) && (table[i][j+3]->get_ID() == ID)){
-                return true;
+    }
+    bool check (int ID){
+        // horizontal
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 4; j++){
+                if((table4[i][j] == ID) && (table4[i][j+1] == ID) && (table4[i][j+2] == ID) && (table4[i][j+3] == ID)){
+                    return true;
+                }
+            }
+        }
+
+        // Vertical
+
+        for(int j = 0; j < 7; j++){
+            for(int i = 0; i < 3; i++){
+                if((table4[i][j] == ID) && (table4[i+1][j] == ID) && (table4[i+2][j] == ID) && (table4[i+3][j] == ID)){
+                    return true;
+                }
+            }
+        }
+
+        // Ascending Diagonal
+        for (int i = 4; i < 6; i++){
+            for(int j = 0; j < 5; j++){
+                if((table4[i][j] == ID) && (table4[i-1][j+1] == ID) && (table4[i-2][j+2] == ID) && (table4[i-3][j+3] == ID)){
+                    return true;
+                }
+            }
+        }
+
+        // Descending Diagonal
+        for (int i = 0; i < 4; i++){
+            for(int j = 0; j < 5; j++){
+                if((table4[i][j] == ID) && (table4[i+1][j+1] == ID) && (table4[i+2][j+2] == ID) && (table4[i+3][j+3] == ID)){
+                    return true;
+                }
+            }
+        }
+
+
+
+
+        return false;
+    }
+    void place (int ID){
+        int x = 0;
+        int row = 5;
+        int col = 0;
+        int player = 1;
+        while((x < 42) && (check(1) != 1) && (check (2) != 1)){
+            cout << "Player " << player << "'s turn! Which column do you want to place your piece in? (1-7)";
+            cin >> col;
+            col = col - 1;
+            int z = 0;
+            row = 5;
+            while(row > 0){
+                if ((col > 6) || (col < 0)){
+                    cout << "Out of range! Pick another number.";
+                    col = 0;
+                    cin >> col;
+                    col = col - 1;
+                }
+                else{
+                    if(table4[0][col] != 0){
+                        print();
+                        cout << "Column is full! Pick another!";
+                        col = 0;
+                        cin >> col;
+                        col = col - 1;
+                    }
+                    else{
+                        row = 5;
+                        int r = 5;
+                        while(r > 0){
+                            if(table4[row][col] != 0){
+                                row--;
+                                r--;
+                            }
+                            else{
+                                r = 0;
+                            }
+                        }
+                        if (table4[row][col] == 0) {
+                            if(player == 1) {
+                                table4[row][col] = 1;
+                                pieces = pieces - 1;
+                            }
+                            else{
+                                table4[row][col] = 2;
+                                pieces--;
+                            }
+                            x++;
+                            if(player == 1){
+                                player = 2;
+                            }
+                            else{
+                                player = 1;
+                            }
+                            print();
+                            break;
+                        }
+                        row--;
+                    }
+                }
+            }
+        }
+        if(check(1) == 1){
+            cout << "Player 1 Wins!" << endl;
+        }
+        else {
+            if (check(2) == 1) {
+                cout << "Player 2 Wins!" << endl;
+            } else {
+                cout << "Board is full! Game over!" << endl;
             }
         }
     }
 
-    // Vertical
-
-    for(int j = 0; j < 7; j++){
-        for(int i = 0; i < 3; i++){
-            if((table[i][j]->get_ID() == ID) && (table[i+1][j]->get_ID() == ID) && (table[i+2][j]->get_ID() == ID) && (table[i+3][j]->get_ID() == ID)){
-                return true;
-            }
-        }
-    }
-
-    // Ascending Diagonal
-    for (int i = 4; i < 6; i++){
-        for(int j = 0; j < 5; j++){
-            if((table[i][j]->get_ID() == ID) && (table[i-1][j+1]->get_ID() == ID) && (table[i-2][j+2]->get_ID() == ID) && (table[i-3][j+3]->get_ID() == ID)){
-                return true;
-            }
-        }
-    }
-
-    // Descending Diagonal
-    for (int i = 0; i < 4; i++){
-        for(int j = 0; j < 5; j++){
-            if((table[i][j]->get_ID() == ID) && (table[i+1][j+1]->get_ID() == ID) && (table[i+2][j+2]->get_ID() == ID) && (table[i+3][j+3]->get_ID() == ID)){
-                return true;
-            }
-        }
-    }
-
-    return false;
 };
+
+
+
+
+
+
+
+
+
 
 
 int main() {
 
 
-    Player emp1 (1,21);
-    Table * table [6][7];
-    table[0][0] = emp1.get_ID();
-
-
-
-    int p = 21;
-    int x = 0;
-
-    Player emp (0,p);
-    while(x < 6){
-        int y = 0;
-        while(y < 7){
-            table[x][y] = &emp;
-            y++;
-        }
-        x++;
-    }
-    //cout << "T: ID: " << table[0][1]->get_ID() <<" . Pieces: " << table[0][1]->get_pieces() << endl;
-
-    //print(table);
-    Player a(1,p);
-    cout << "A: ID: " << a.get_ID() << ". Pieces: " << a.get_pieces() << endl;
-    Player b (2,p);
-
-    cout << "B: ID: " << b.get_ID() << ". Pieces: " << b.get_pieces() << endl;
-    //  cout << "E: ID: " << empty.get_ID() << ". Pieces: " << empty.get_pieces() << endl;
-    // cout << "T: ID: " << table[0][1]->get_ID() <<" . Pieces: " << table[0][1]->get_pieces() << endl;
-
-
-    x = 0;
-    int same = 0;
-    int row = 5;
-    int col = 0;
-    int player = 1;
-    while ((x < 42) && (check(a.get_ID()) == 0) && (check(b.get_ID()) == 0)){
-        cout << "Player " << player << "'s turn! Which column do you want to place your piece in? (1-7)";
-        cin >> col;
-        col = col - 1;
-        int z = 0;
-        row = 5;
-        while(row > 0) {
-            if ((col > 6) || (col < 0)) {
-                cout << "Out of range! Pick another number.";
-                col = 0;
-                cin >> col;
-                col = col - 1;
-            } else {
-                if (table[0][col]->get_ID() != 0) {
-                    cout << "Column is full! Pick another!";
-                    col = 0;
-                    cin >> col;
-                    col = col - 1;
-                } else {
-                    row = 5;
-                    int r = row;
-                    while(r > 0){
-                        if(table[row][col]->get_ID() != emp.get_ID()){
-                            row--;
-                            r--;
-                        }
-                        else{
-                            r = 0;
-                        }
-                    }
-                    if (table[row][col]->get_ID() == emp.get_ID()) {
-                        if(player == 1) {
-                            table[row][col] = &a;
-                            a--;
-                        }
-                        else{
-                            table[row][col] = &b;
-                            b--;
-                        }
-                        x++;
-                        if(player == 1){
-                            player = 2;
-                        }
-                        else{
-                            player = 1;
-                        }
-                        print(table);
-                        break;
-                    }
-                    row--;
-                }
-
-            }
-        }
-    }
-
-    if(check(a.get_ID()) == 1){
-        cout << "Player 1 Wins!" << endl;
-    }
-    else {
-        if (check(b.get_ID()) == 1) {
-            cout << "Player 2 Wins!" << endl;
-        } else {
-            cout << "Board is full! Game over!" << endl;
-        }
-    }
-
+    Table4 t(1);
+    t.initialize();
+    t.print();
+    t.place(1);
 
 
     return 0;
 }
-
